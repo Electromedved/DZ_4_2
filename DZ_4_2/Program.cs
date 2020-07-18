f3e5a3555d463434e1d7d983e94f93c9af726f02
Виталий Анатольевич <pigcontrol@gmail.com>
2020-07-18 17:39:58 +0300

    Add project files.

---
+++ DZ_4_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_4_2
+{
+    //2. Заполнить массив из десяти элементов значениями, вводимыми с клавиатуры в ходе выполнения программы.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int[] numbArr = new int[10];
+
+            for (int i = 0; i < numbArr.Length; i++)
+            {
+                Console.WriteLine($"Введите элемент массива #{i + 1}:");
+                numbArr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("--------------------------------------------------");
+
+            foreach (int i in numbArr)
+            {
+                Console.Write($"{i} ");
+                //Console.WriteLine(numbArr[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
